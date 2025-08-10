1f358d971b7280ddbf06ea6c07256945b15d8ebc
Riken-sajal <riken@sajaltech.com>
2025-08-10 15:20:54 +0530

    try diff ways for static

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-vercel-build-command
+
+# Copy collected static files to a public directory Vercel can serve
+mkdir -p public/static
+cp -r staticfiles/* public/static/
